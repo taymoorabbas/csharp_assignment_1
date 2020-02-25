37e3bbd9cc3f28e9ededb74dd4bb58a5fcd90156
taymoorabbas <www.taymoor@gmail.com>
2020-02-26 03:44:10 +0500

    part: 1 solution

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace assignment_1
     {
         static void Main(string[] args)
         {
-            
+            int[] numbers = {80, 1, 56, 78, 45, 21, 69, 81, 55, 83, 32, 15};
+            Console.WriteLine("2nd largest number in given 1D array is: " + get_largest(numbers));
+        }
+
+        static int get_largest(int[] array){
+
+            int largest = array[0];
+            int second_largest = array[0];
+
+            foreach(int item in array){
+
+                if(item > largest){
+
+                    second_largest = largest;
+                    largest = item;
+                }
+            }
+
+            return second_largest;
         }
     }
 }
